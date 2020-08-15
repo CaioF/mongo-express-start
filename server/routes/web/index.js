0466b42cf834ac7c153153b3ba9f06febe417f3e
Caio <Fleury>
2020-08-15 16:29:15 +0300

    Fixed linting issues

--- server/routes/web/index.js
+++ server/routes/web/index.js
@@ -3,16 +3,16 @@ web/index.js mounts all routes inside the web folder
 these routes are mounted in ../server.js on the route '/'
 */
 const express = require('express');
-//const path = require('path');
+// const path = require('path');
 
 const router = express.Router();
-//const path2static = path.join(__dirname, '../../../app/example/');
+// const path2static = path.join(__dirname, '../../../app/example/');
 
 router.get('/', (req, res, next) => {
   res.json({
     message: 'Web API is working'
   });
-  //res.sendFile(`${path2static}devcycle.png`);
+  // res.sendFile(`${path2static}devcycle.png`);
   next();
 });
 
